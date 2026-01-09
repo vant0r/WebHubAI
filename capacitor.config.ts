b7c8b8420c36dee3be9e9ed56a34a04397191ceb
Vantor <vantorcyber@gmail.com>
2026-01-09 19:17:04 +0500

    Ensure workflow triggers

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ai.webhub.app',
+  appName: 'WebHubAI',
+  webDir: 'dist'
+};
+
+export default config;
